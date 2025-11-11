9048e0ec1b17f8e69c11dfc7c3f141f926e92907
Chika Egulemu <chikaegus024@gmail.com>
2025-11-11 14:08:37 +0100

    ;env added and mpn run dev

---
+++ backend/src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+// import onboardingRoutes from "./routes/onboarding";
+
+dotenv.config();
+console.log("PORT from .env:", process.env.PORT);
+
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// app.use("/api/onboarding", onboardingRoutes);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
