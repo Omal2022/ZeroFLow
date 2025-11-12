4a1da3ff3bf744639d275f73ed24ee7f88794e4a
Chika Egulemu <chikaegus024@gmail.com>
2025-11-12 02:02:09 +0100

    dont know

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,8 +3,6 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 
-import onboardingRoutes from "./routes/onboarding";
-
 dotenv.config();
 console.log("PORT from .env:", process.env.PORT);
 
@@ -12,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use("/api/onboarding", onboardingRoutes);
+// app.use("/api/onboarding", onboardingRoutes);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
